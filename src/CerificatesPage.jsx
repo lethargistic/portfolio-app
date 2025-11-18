50c85d98e2ae96536582caf0779fa905ffd5408c
Maksiks <maksiksking@gmail.com>
2025-11-18 12:22:54 +0200

    chore: yet another diploma today

--- src/CerificatesPage.jsx
+++ src/CerificatesPage.jsx
@@ -43,7 +43,8 @@ const imgSources = [
     {src: "/img/cert/IMG_20251022_200035.webp", size: "5x4"},
     {src: "/img/cert/IMG_20251022_200141.webp", size: "5x2"},
     {src: "/img/cert/IMG_20251022_195734.webp", size: "5x4"},
-    {src: "/img/cert/diploma171125.webp", size: "5x4"},
+    {src: "/img/cert/webdevagain.webp", size: "5x4"},
+    {src: "/img/cert/trivia2ndplace.webp", size: "5x4"},
 ]
 
 let currentLightboxSrc = "oh";
