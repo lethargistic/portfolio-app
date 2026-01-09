cce71f462b2da2618f7513f1e123d21d4291994b
Maksiks <maksiksking@gmail.com>
2026-01-09 13:26:21 +0200

    feat: dynamic mask-image cutouts

--- src/lib/segments/SegLinktree.svelte
+++ src/lib/segments/SegLinktree.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
     import {currentLang} from "$lib/shared.svelte";
-    import {Tween} from "svelte/motion";
 
-    import * as three from "three";
     import {onMount} from "svelte";
-    import {cubicOut} from "svelte/easing";
     import Chime from "$lib/hangies/Chime.svelte";
     import {SeparatorShape} from "$lib/utils/utils";
 
@@ -51,61 +48,6 @@
     onMount(async () => {
         await updateSocials();
     })
-
-    //
-
-    let canvas: HTMLCanvasElement | null = $state(null);
-
-    const fov = 75;
-    const aspect = 2;
-    const near = 0.1;
-    const far = 5;
-
-    const cubeRotationSettings = {
-        duration: 2000,
-        easing: cubicOut,
-    };
-
-    let cubeRotation = $state(new Tween(0, cubeRotationSettings));
-
-    let cube: three.Mesh | null = $state(null);
-    let renderer: three.WebGLRenderer | null = $state(null);
-    let scene: three.Scene | null = $state(null);
-    let camera: three.Camera | null = $state(null);
-
-    $effect(() => {
-        if (!cube || !renderer || !scene || !camera) return;
-        cube.rotation.x = cubeRotation.current;
-        cube.rotation.y = cubeRotation.current;
-        renderer.render(scene, camera)
-        if (cubeRotation.current >= Math.PI * 2) {
-            cubeRotation = new Tween(-(Math.PI * 2), cubeRotationSettings);
-            cubeRotation.target = Math.PI * 2;
-        }
-    })
-
-    onMount(() => {
-        if (canvas == null) return;
-        renderer = new three.WebGLRenderer({antialias: true, canvas});
-        camera = new three.PerspectiveCamera(fov, aspect, near, far);
-        camera.position.z = 2;
-
-        scene = new three.Scene();
-
-        const boxWidth = 1;
-        const boxHeight = 1;
-        const boxDepth = 1;
-        const boxGeometry = new three.BoxGeometry(boxWidth, boxHeight, boxDepth);
-        const boxMaterial = new three.MeshBasicMaterial({color: 0xffffff});
-
-        cube = new three.Mesh(boxGeometry, boxMaterial);
-
-        scene.add(cube);
-
-        renderer.render(scene, camera);
-
-        cubeRotation.target = Math.PI * 2;
-    })
 </script>
 
 
@@ -116,8 +58,8 @@
         <div class=chime-cont>
             {#each socials as social}
                 {#if !isSocialHidden(social)}
-                    <div class="social-chime">
-                        <Chime {social} folds={social.folds} foldCount={4} chimeYOffset={0.15} chimeHeightVh="82vh"
+                    <div class={`social-chime social-chime-${social.name}`}>
+                        <Chime {social} folds={social.folds} foldCount={social.fold_count} chimeYOffset={0.15} chimeHeightVh={82}
                                separatorShape={SeparatorShape.ThreeStars}></Chime>
                     </div>
                 {/if}
@@ -138,8 +80,8 @@
 
             background-color: white;
             /* bg for testing */
-            /*background-image: url("/img/train_front.webp");*/
-            /*background-repeat: repeat;*/
+            background-image: url("/img/train_front.webp");
+            background-repeat: repeat;
 
             & .lilac-cherry-branch {
                 width: 84vw;
@@ -155,10 +97,18 @@
 
                 & .social-chime {
                     position: absolute;
-                    transform: translate(-37%, 0);
-                    left: 37vw;
                     /* TODO: make dynamic */
                     top: 58vh;
+                }
+
+                & .social-chime-github {
+                    transform: translate(-37%, 0);
+                    left: 37vw;
+                }
+
+                & .social-chime-chaos-abyss {
+                    transform: translate(-67%, 0);
+                    left: 67vw;
                 }
             }
         }
