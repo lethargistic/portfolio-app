b7e7b35e327f532454ece6deb11d61143269edbc
Maksiks <maksiksking@gmail.com>
2026-01-30 18:26:59 +0200

    feat: error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -10,6 +10,9 @@
             Never forgetti<br><span class="tini"> - HGModernism, 2025</span>
         </p>
     </div>
+    <div class="img-cont">
+        <img src="/img/rest-in-spaghetti.svg" alt="rest in spaghetti">
+    </div>
 </div>
 
 <style>
@@ -19,8 +22,13 @@
         background-color: #232323;
         color: white;
 
-        display: grid;
+        display: flex;
         place-items: center start;
+
+        ::selection {
+            color: black;
+            background-color: white;
+        }
 
         & .cont-cont {
             display: grid;
@@ -43,6 +51,17 @@
                     font-size: 1.7rem;
                     margin-left: auto;
                 }
+            }
+        }
+
+        & .img-cont {
+            width: 100%;
+            display: grid;
+            place-items: center;
+
+            img {
+                width: 70%;
+                user-select: none;
             }
         }
     }
